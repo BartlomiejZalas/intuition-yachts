3854844988eef60dc0f381904347bb4df7d12e4b
Bartlomiej Zalas <bartekzalas@gmail.com>
2018-11-18 22:57:59 +0200

    aux fix

--- src/pages/projects/Project.js
+++ src/pages/projects/Project.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import AuxC from '../../components/Aux';
+import AuxC from '../../components/AuxC';
 import { translate } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
