059b5d5c5b288529d11f364eeef43a31801d304a
Bartlomiej Zalas <bartekzalas@gmail.com>
2018-11-27 19:47:09 +0200

    about us updated

--- src/i18n.js
+++ src/i18n.js
@@ -33,23 +33,44 @@ i18n
           pages: {
             about: {
               title: 'O Nas', content: `
-            <p>Intuition Yachts jest to działalność założona przez 
-            absolwentkę Wydziału Oceanotechniki i Okrętownictwa Politechniki Gdańskiej - Martę Annę 
-            Zawadzką.</p>
+              <p>Każda historia ma swój początek. Intuation Yachts swoją historię opiera na pasji do jachtów,
+              która rozpoczęła się w 2012 roku. W tym właśnie czasie założycielka firmy podczas edukacji
+              natknęła się na jednostkę „Intuition” i jej głównego projektanta, słynnego Juliusza
+              Strawińskiego. Przyjął ją pod swoje skrzydła i zaraził niesłabnącą pasją do projektowania.</p>
 
-            <p>Dzięki pasji, wykształceniu oraz zdobytemu doświadczeniu możemy zaoferować Państwu 
-            profesjonalne usługi przy użyciu najlepszej jakości sprzętu oraz specjalistycznemu 
-            oprogramowaniu. Używamy programów takich jak: AutoCAD, Rhinoceros,  V-Ray, 
-            Flamingo, Orca 3D, SSC oraz MultiSurf.</p>
-            
-            <p>Dzięki zdobywaniu doświadczenia zaróno w kraju jak i za granicą jesteśmy w stanie spełnić 
-            wymagania różnego rodzaju klientów, oferując przy tym szeroki pakiet usług.</p>
-            
-            <p>Projekty realizujemy na terenie Polski oraz za granicą w zależności od wymagań klienta.
-            <br>
-            Część wykonanych projektów podczas zdobywania doświadczenia można obejrzeć w naszym Portfolio:</p>
-            
-            <a href="#">TU BĘDZIE PDF DO ŚCIĄGNIĘCIA</a>` },
+              <p>Po latach nauki, firma gotowa jest aby samodzielnie spełniać potrzeby klientów przez
+              świadczenie kompleksowych usług projektowania i modelowania.</p>
+
+              <p>Misją i największym marzeniem organizacji jest to, aby każdy zaprojektowany jacht został
+              jednostką przemierzającą wody świata. Czy będzie to Twój jacht?</p>
+
+              <p>Nasz zespół składa się z doświadczonych inżynierów o szerokim zakresie wiedzy technicznej
+              i nietuzinkowej kreatywności. W rezultacie jesteśmy w stanie dopasować zespół
+              profesjonalistów do potrzeb klienta, tak aby sprostać nawet najbardziej skomplikowanym
+              projektom. Nasi inżynierowie mogą również wspierać naszego klienta podczas prefabrykacji
+              jako nadzór. Zarówno podczas nowej budowy, jak i podczas przebudowy jednostek.</p>
+
+              <p>Zaufaj nam i pozwól nam się poznać. Poniżej znajdziesz nasze portfolio:<br>
+              <a href="#">TU BĘDZIE PDF DO ŚCIĄGNIĘCIA</a></p>
+              
+              <p>Założycielka:
+              Marta Anna Zawadzka- Założycielka „Intuition Yachts”. Zdobyła dwa tytuły na Politechnice
+              Gdańskiej z zakresu projektowania oraz zarządzania w gospodarce morskiej. Jachty są jej
+              hobby i zarazem pracą. Odbyła liczne praktyki zawodowe zarówno w kraju jak i za granicą w
+              takich firmach jak: Stocznia Gdynia S.A., Krajowa Izba Gospodarki Morskiej, PRS, Surge-
+              projects i w szczególności Yacht Studio Strawiński.</p>
+              
+              <p>Aby poszerzyć horyzonty, przeniosła się do jednej z europejskiej stolicy jachtów - Majorki.
+              Zdobyła tam doświadczenie w firmie zarządzającej projektami budowy jachtów, a następnie
+              próbowała swoich sił w projektowaniu ich wnętrz. Dzięki tym doswiadczeniom dała wyraz
+              swojemu wyczuciu stylu oraz rozwinęła zamiłowanie do eleganckich i luksusowych
+              wystrojów wnętrz.</p>
+
+              <p>Pasja do żeglowania i spędzone dzięki temu godziny na morzu pozwalają Marcie poznawać
+              poszczególne jednostki od strony użytkownika. Dzięki temu projekty są nie tylko
+              przemyślane wizualnie i technicznie, ale mają również na celu ergonomię oraz ciągłe
+              doskonalenie standardowo stosowanych rozwiązań.</p>
+            ` },
             services: {
               title: 'Usługi', content: `
             <ul>
