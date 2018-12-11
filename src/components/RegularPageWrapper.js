7e1f5cb1112ec0eb1031109a912a976776f8705c
Bartlomiej Zalas <bartekzalas@gmail.com>
2018-12-11 21:55:20 +0200

    slider added

--- src/components/RegularPageWrapper.js
+++ src/components/RegularPageWrapper.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./RegularPageWrapper.css";
+import Slider from "./Slider/Slider";
 
 const RegularPageWrapper = ({ children, title }) => (
     <div>
-        <div className="fixed-width">
-            <div className="page-photo" style={{ backgroundImage: `url(${require("./boats.jpg")})` }}></div>
+        <div className="container" style={{ background: 'rgba(255,255,255, 0.85)' }}>
+            <div className="row">
+                <div className="col">
+                    <Slider />
+                </div>
+            </div>
         </div>
-        <div className="container" style={{background: 'rgba(255,255,255, 0.85)'}}>
+        <div className="container" style={{ background: 'rgba(255,255,255, 0.85)' }}>
             <div className="row">
                 <div className="col py-3">
                     <h2 className="display-4 py-3">{title}</h2><hr />
@@ -17,10 +22,10 @@ const RegularPageWrapper = ({ children, title }) => (
         <footer className="footer text-center">
             <div className="container py-3">
                 <hr />
-                <small className="text-muted">Copyright &copy; Intuition Yachts Design Studio 2018</small>
+                <small className="text-muted">Copyright &copy; Intuition Yachts Design Studio {new Date().getFullYear()}</small>
             </div>
         </footer>
-    </div>
+    </div >
 );
 
 export default RegularPageWrapper;
