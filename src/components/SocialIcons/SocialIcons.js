4684c83289371029c7a56f800d375f8e1298fc25
Bartlomiej Zalas <bartekzalas@gmail.com>
2018-12-11 20:49:42 +0200

    social links updated

--- src/components/SocialIcons/SocialIcons.js
+++ src/components/SocialIcons/SocialIcons.js
@@ -4,8 +4,15 @@ import './SocialIcons.css';
 
 const socialIcons = () => (
     <div className="social-icons">
-        <a href="http://facebook.com" className="facebook"><i className="fa fa-facebook"></i></a>
-        <a href="http://linkedin.com" className="linkedin"><i className="fa fa-linkedin"></i></a>
+        <a href="https://www.facebook.com/IntuitionYachtsDesignStudio" className="facebook" target="blank">
+            <i className="fa fa-facebook"></i>
+        </a>
+        <a href="https://www.linkedin.com/company/intuition-yachts" className="linkedin" target="blank">
+            <i className="fa fa-linkedin"></i>
+        </a>
+        <a href="https://www.instagram.com/intuition_yachts" className="instagram" target="blank">
+            <i className="fa fa-instagram"></i>
+        </a>
     </div>
 );
 
